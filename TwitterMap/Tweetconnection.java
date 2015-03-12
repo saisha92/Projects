63571aa89df8d7fc2b8209c2ab72bbbd7abb57a6
Sai Shankar Sumukar <saisha.sumukar@gmail.com>
2015-03-11 22:08:40 -0400

    Update Tweetconnection.java

--- TwitterMap/Tweetconnection.java
+++ TwitterMap/Tweetconnection.java
@@ -10,47 +10,52 @@ import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-public class Tweetcollection {
-	final static int tweetmax = 10000;
-	 static int tweetcount = 0;
+public class ConnectionCheck {
+	final static int max = 10000;
+	 static int count = 0;
 	public static void main(String[] args)
 	{
 		
 	try{
 	
-		
+		System.out.print("Successful connection \n");
+	
 		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setOAuthConsumerKey("")
-        .setOAuthConsumerSecret("")
-        .setOAuthAccessToken("")
-        .setOAuthAccessTokenSecret("");
-		final TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance(); 
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("")
+		  .setOAuthConsumerSecret("")
+		  .setOAuthAccessToken("")
+		  .setOAuthAccessTokenSecret("")
+		  .setJSONStoreEnabled(true);
+		final TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+	 
+        
         StatusListener listener = new StatusListener() {
         DBConnection db = new DBConnection();
         	Connection conn = db.createConnection();
             
             public void onStatus(Status status) {
             	
-            	if(tweetcount<tweetmax)
+            	if(count<max)
             	{ 
             		if(status.getId()!=0 && status.getGeoLocation()!=null&&status.getLang().equalsIgnoreCase("en"))
                 {
             		System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
             		PreparedStatement preparedStatement=null;
             		try {
-						preparedStatement = conn.prepareStatement("insert into tweets values(?,?,?,?)");
-						
-						preparedStatement.setString(1,status.getUser().getScreenName() );
+						preparedStatement = conn.prepareStatement("insert into tweetinfo values(?,?,?,?,?)");
+						preparedStatement.setString(1, Long.toString(status.getId()));
 						preparedStatement.setDouble(2, status.getGeoLocation().getLatitude());
 						preparedStatement.setDouble(3, status.getGeoLocation().getLongitude());
-						preparedStatement.setString(4, status.getText());
+						preparedStatement.setString(4,status.getUser().getScreenName() );
+						preparedStatement.setString(5, status.getText());
 						preparedStatement.executeUpdate();
 					} catch (SQLException e) {
-						
+						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
             		
-            	tweetcount++;
+            	count++;
                 }
             	}
             	else{
